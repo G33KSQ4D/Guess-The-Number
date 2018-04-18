137f0c1ab25d32580e80d0ed598f242c83404851
Jonathan Purcell <jonathan_purcell@icloud.com>
2018-04-18 19:56:19 +0000

    I tried. I have made a mess. Time to delete the code and restart.

--- Networking/two_user_chat/client.py
+++ Networking/two_user_chat/client.py
@@ -1,29 +1,88 @@
 # Created April 17th, 2018
 
+"""
+1. Create socket
+2. Connect to host
+3. Start multithreading processes for chattin between
+   the two people
+"""
+
 import socket
+import threading
+import traceback
 
 def main():
+	# Incase the user is a moron
+    while True:
+        try:
+            print("How to format -> host:port")
+            host_port_list = input("Enter the address and port (Check above for how to format it): ").split(":")
+            host_port_list[1] = int(host_port_list[1])
+
+            if len(host_port_list) == 2:
+                # 3 numbers * 4 times + 3 dots (Ex: 192.168.0.101)
+                # host_port_list[0] == host name len ipv4 is always greater than 12
+                if 7 <= len(host_port_list[0]) <= 3*4+3:
+                    if 0 < host_port_list[1] < 65535:
+                        print("Alright, trying to connect to server...\n")
+                        host = host_port_list[0]
+                        port = host_port_list[1]
+                        
+                        try:
+                        	# This function will call other functions
+                        	client_socket = initialize_client_socket(host, port)
+
+                        except Exception:
+                        	print("Something went wrong... Exiting")
+                        	exit(1)
+
+                    else:
+                        raise Exception("Port number out of range")
+                else:
+                    raise Exception("Invalid IP Address")
+            else:
+                raise Exception("Invalid print format")
+
+        except Exception:
+            print(traceback.format_exc())
+            continue
+
+def initialize_client_socket(host, port):
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# These are temporary of course
-	server_ip   = "0.0.0.0"
-	server_port = 1400
-	packet_size = 2048
 
-	client_socket.connect( (server_ip, server_port) )
+	client_socket.connect( (host, port) )
 
-	# So the client_sock doesn't crash
+	# Multithreading (Read + write)
+	get_incoming_packets_thread = threading.Thread(target=get_incoming_packets, args=(client_socket))
+	send_client_packets_thread  = threading.Thread(target=send_client_packets_thread, args=(client_socket))
+
+	get_incoming_packets_thread.start()
+	send_client_packets_thread.start()
+
+def get_incoming_packets(client_socket):
+	packet_byte_size = 2048
+
 	while True:
 		try:
-			# The data will be encoded with utf-8 when it arrives
-			server_data = (client_socket.recv(packet_size).decode("utf-8"))
-			print(server_data)
+			data = (client_socket.recv(packet_byte_size)).decode("utf-8")
+			print(data)
 
 		except:
-			print("The server has shutdown. Disconnecting from server...")
+			print("An error occured. Closing server...\n")
 			client_socket.close()
 			exit(1)
 
 
+def send_client_packets(client_socket):
+	while True:
+		try:
+			data = input("> ").encode("utf-8")
+			client_socket.send(data)
+
+		except:
+			print("An error occured. Closing server...\n")
+			client_socket.close()
+			exit(1)		
 
 if __name__ == "__main__":
 	main()
--- Networking/two_user_chat/server.py
+++ Networking/two_user_chat/server.py
@@ -1,34 +1,108 @@
 # Started April 17th, 2018
 
+# Although this isn't really a server, that techincally
+# is what the job of this script is.
+
+# If you are going to use these scripts for some reason,
+# Don't forget to enable port forwarding in your router settings
+# For whatever port you choose
+
+"""
+1. Create socket
+2. Bind - Listen - Accept -> connections
+3. Start multithreading process for chatting
+   between the two people
+4. Add a username
+"""
+
 import socket
 import threading 
 
 def main():
-	server_socket      = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-	# These are temporary of course
-	server_ip   	   = "0.0.0.0"
-	server_port 	   = 1400
-	server_max_clients = 2 # This program is for 
+	# I don't want to clutter this function
 
-	# Bind the socket to a port on an ip
-	server_socket.bind( (server_ip, server_port) )
+	# Create socket
+	host_socket = initialize_host_socket()
 
-	# Start listening for incoming requests
-	server_socket.listen(server_max_clients)
+	# Look for a connection
+	# ** This function will call other functions **
+	accept_a_client(host_socket)
 
-	# Make sure the server_sock actually initialized
-	if not server_socket:
-		print("Error creating server socket. Exiting..")
-		exit(1)
+def initialize_host_socket():
+	"""
+	Initialize a socket for the host, make sure it was successful and start listening for a connection
+	"""
+	host_socket     = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	host_ip 	    = "0.0.0.0"
+	host_port 	    = 1337
+	# This program is only for one person
+	max_connections = 1
+
+	# Attach the socket to a port on the host's machine
+	host_socket.bind( (host_ip, host_port) )
+
+	# Making sure the socket was created properly and is working
+	if host_socket:
+		print("Socket created successfully!\nListening for an incoming connection...\n")
+		# Start listening for incoming connection
+		host_socket.listen(max_connections)
 	else:
-		print("Server started at {0}:{1}".format(server_ip, server_port))
+		print("Error initializing the socket. Exiting.")
+		host_socket.close()
+		exit(1)
 
+	return host_socket
+
+def accept_a_client(host_socket):
+	# Socket connection and IPv4 of the client
+	connection, addr = host_socket.accept()
+
+	# At this point, a connection was established 
+	while True: # Keep the socket open until someone closes it
+		# Need to multithread (To read + write)
+		read_incoming_data_thread  = threading.Thread(target=read_incoming_data,  args=(connection, connection, addr))
+		send_data_to_client_thread = threading.Thread(target=send_data_to_client, args=(connection, connection, addr)) 
+
+		# Start the threads 
+		read_incoming_data_thread.start()
+		send_data_to_client_thread.start()
+
+
+def read_incoming_data(host_socket, connection, addr):
+	packet_byte_size = 2048
 	while True:
-		client_connection, client_address = server_socket.accept()
-		print("{0} joined the server...".format(client_address))
+		try:
+			data = (connection.recv(packet_byte_size)).decode("utf-8")
+			
+			if len(data) <= 0:
+				print("Client has disconnected")
+				host_socket.close()
+				exit(1)
 
-		# Test message
-		client_connection.send("This is a test".encode(encoding="utf-8"))
+			else:
+				print(data)
+		
+		except:
+			print("An error occured. Closing server...\n")
+			host_socket.close()
+			exit(1)
+
+def send_data_to_client(host_socket, connection, addr):
+	while True:
+		try:
+			data_to_send = input("> ").encode(encoding="utf-8")
+
+			if data_to_send.decode("utf-8") == "quit":
+				print("Closing the host socket.. Exiting.")
+				host_socket.close()
+				exit(1)
+			else:
+				connection.send(data_to_send)
+
+		except:
+			print("An error occured. Closing server...\n")
+			host_socket.close()
+			exit(1)
 
 if __name__ ==  "__main__":
 	main()
